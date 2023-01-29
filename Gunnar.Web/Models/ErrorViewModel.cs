eb66434cd3b1782fa2390c5343399117cce22c5f
Abiratan Lopes <abi.lopes.sousa@gmail.com>
2023-01-29 10:23:57 -0300

    Initial commit.

---
+++ Gunnar.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Gunnar.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
